95858ed7e301179e108dc3f7837f2e8a5c8e82f7
SariKerim <ks146@hdm-stuttgart.de>
2015-05-05 15:03:22 +0200

    Initial commit for the it-project.

---
+++ ITProjekt/src/de/hdm/itprojekt/client/ITProjekt.java
@@ -0,0 +1,16 @@
+package de.hdm.itprojekt.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class ITProjekt implements EntryPoint {
+
+	/**
+	 * This is the entry point method.
+	 */
+	public void onModuleLoad() {
+		
+	}
+}
